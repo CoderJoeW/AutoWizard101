67b8363a1a87086779459b9e3f29c55d49fe06a6
jwilliamson <j.williamson@gambler-labs.com>
2022-05-14 20:34:20 -0400

    switch to launch halfang bot on start

--- ProjectMaelstrom/ProjectMaelstrom/Program.cs
+++ ProjectMaelstrom/ProjectMaelstrom/Program.cs
@@ -11,7 +11,7 @@ namespace ProjectMaelstrom
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new BazaarReagentBot());
+            Application.Run(new HalfangFarmingBot());
         }
     }
 }
