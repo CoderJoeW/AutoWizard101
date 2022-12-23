5a7fccd6ed99f307b33b566331368ea1536a7328
jwilliamson <j.williamson@gambler-labs.com>
2022-12-23 16:52:13 -0500

    player controller class for actions player does

---
+++ ProjectMaelstrom/ProjectMaelstrom/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace ProjectMaelstrom
+{
+    internal class PlayerController
+    {
+        private InputSimulator _inputSimulator;
+
+        public void MoveForward()
+        {
+            _inputSimulator.Keyboard.KeyDown(VirtualKeyCode.UP);
+        }
+
+        public void MoveBackward()
+        {
+            _inputSimulator.Keyboard.KeyDown(VirtualKeyCode.BACK);
+        }
+
+        public void MoveLeft()
+        {
+            _inputSimulator.Keyboard.KeyDown(VirtualKeyCode.LEFT);
+        }
+
+        public void MoveRight()
+        {
+            _inputSimulator.Keyboard.KeyDown(VirtualKeyCode.RIGHT);
+        }
+
+        public void Interact()
+        {
+            _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_X);
+        }
+
+        public void Click(Point clickPoint)
+        {
+            WinAPI.click(clickPoint);
+        }
+    }
+}
