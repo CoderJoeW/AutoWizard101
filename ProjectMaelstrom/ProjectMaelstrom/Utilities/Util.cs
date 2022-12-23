fdcb141561d585ab693f26ee59a878b3274072f6
jwilliamson <j.williamson@gambler-labs.com>
2022-12-23 16:59:29 -0500

    create a base class for utils to prevent duplicate code

---
+++ ProjectMaelstrom/ProjectMaelstrom/Utilities/Util.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMaelstrom.Utilities
+{
+    internal class Util
+    {
+        internal ImageRecognition _imageRecognition = new ImageRecognition();
+        internal PlayerController _playerController = new PlayerController();
+    }
+}
