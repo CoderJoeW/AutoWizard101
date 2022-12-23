187807b42e4e5e5b0dd66b956e155de2e2d68b8a
jwilliamson <j.williamson@gambler-labs.com>
2022-12-23 16:49:11 -0500

    moved resolution to constants

---
+++ ProjectMaelstrom/ProjectMaelstrom/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMaelstrom
+{
+    internal class Constants
+    {
+        public const string RESOLUTION = "1024x768";
+    }
+}
