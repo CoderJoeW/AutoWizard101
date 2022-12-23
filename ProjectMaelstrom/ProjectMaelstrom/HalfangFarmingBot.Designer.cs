eb2d883809fbe4a90cb1903f3d0c9af494f3e17f
jwilliamson <j.williamson@gambler-labs.com>
2022-12-23 16:52:58 -0500

    added some temporary labels just so I can make sure things work how i expect

--- ProjectMaelstrom/ProjectMaelstrom/HalfangFarmingBot.Designer.cs
+++ ProjectMaelstrom/ProjectMaelstrom/HalfangFarmingBot.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.botState = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -41,21 +43,44 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "State";
+            // 
+            // botState
+            // 
+            this.botState.AutoSize = true;
+            this.botState.Location = new System.Drawing.Point(65, 47);
+            this.botState.Name = "botState";
+            this.botState.Size = new System.Drawing.Size(38, 15);
+            this.botState.TabIndex = 2;
+            this.botState.Text = "label2";
+            // 
             // HalfangFarmingBot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(241, 138);
+            this.Controls.Add(this.botState);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "HalfangFarmingBot";
             this.Text = "HalfangFarmingBot";
             this.Load += new System.EventHandler(this.HalfangFarmingBot_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button button1;
+        private Label label1;
+        private Label botState;
     }
 }
