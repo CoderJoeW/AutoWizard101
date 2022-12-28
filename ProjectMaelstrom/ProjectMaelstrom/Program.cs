5ca1d247be40e2842dad7e66cf2e9e1b79b14bde
jwilliamson <j.williamson@gambler-labs.com>
2022-12-28 16:56:17 -0500

    just some testing

--- ProjectMaelstrom/ProjectMaelstrom/Program.cs
+++ ProjectMaelstrom/ProjectMaelstrom/Program.cs
@@ -19,6 +19,7 @@ namespace ProjectMaelstrom
             ApplicationConfiguration.Initialize();
             //Application.Run(new HalfangFarmingBot());
             Application.Run(new Main());
+            //Application.Run(new testing());
         }
     }
 }
