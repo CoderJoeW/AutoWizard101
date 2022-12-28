df0cbbddcc24ac9a6034afe2bbd951bbeb95682b
jwilliamson <j.williamson@gambler-labs.com>
2022-12-28 17:01:43 -0500

    add storage utils class

---
+++ ProjectMaelstrom/ProjectMaelstrom/Utilities/StorageUtils.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMaelstrom.Utilities
+{
+    internal class StorageUtils
+    {
+        public static string GetAppPath()
+        {
+            return $"Resources/{StateManager.Instance.SelectedResolution}";
+        }
+    }
+}
