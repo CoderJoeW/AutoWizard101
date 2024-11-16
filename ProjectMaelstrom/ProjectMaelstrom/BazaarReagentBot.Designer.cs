a22146bb7fae141110e736f6e725eaa7249fe456
Joseph Williamson <j.williamson@gambler-labs.com>
2024-11-15 22:46:37 -0500

    add bazaar bot

---
+++ ProjectMaelstrom/ProjectMaelstrom/BazaarReagentBot.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ProjectMaelstrom
+{
+    partial class BazaarReagentBot
+    {
+        private System.ComponentModel.IContainer components = null;
+        private ListBox listBox1;
+        private Label label1;
+        private Button btn;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            label1 = new Label();
+            btn = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(12, 37);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(245, 404);
+            listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(231, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Select Reagents To Scan For";
+            // 
+            // btn
+            // 
+            btn.Location = new Point(289, 37);
+            btn.Name = "btn";
+            btn.Size = new Size(100, 40);
+            btn.TabIndex = 2;
+            btn.Text = "Start Bot";
+            btn.UseVisualStyleBackColor = true;
+            btn.Click += startButton_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(397, 184);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 25);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // BazaarReagentBot
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Controls.Add(btn);
+            Name = "BazaarReagentBot";
+            Text = "BazaarReagentBot";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+    }
+}
---
+++ ProjectMaelstrom/ProjectMaelstrom/BazaarReagentBot.cs
@@ -0,0 +1,156 @@
+﻿using ProjectMaelstrom.Modules.ImageRecognition;
+using ProjectMaelstrom.Utilities;
+using System.Data;
+using System.Drawing.Text;
+using System.Threading;
+using System.Timers;
+
+namespace ProjectMaelstrom
+{
+    public partial class BazaarReagentBot : Form
+    {
+        private System.Timers.Timer _runTimer;
+
+        private bool _botStarted = false;
+
+        private bool _isRunning = false;
+
+        public BazaarReagentBot()
+        {
+            InitializeComponent();
+            LoadPngFiles();
+        }
+
+        private void LoadPngFiles()
+        {
+            string directoryPath = $"{StorageUtils.GetAppPath()}/Bazaar/Reagents";
+
+            if (Directory.Exists(directoryPath))
+            {
+                var fileNames = Directory.GetFiles(directoryPath, "*.png")
+                                         .Select(Path.GetFileNameWithoutExtension)
+                                         .ToList();
+
+                listBox1.SelectionMode = SelectionMode.MultiSimple;
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(fileNames.ToArray());
+            }
+            else
+            {
+                MessageBox.Show("Directory not found: " + directoryPath, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (!_botStarted)
+            {
+                _runTimer = new System.Timers.Timer(TimeSpan.FromSeconds(1));
+                _runTimer.Elapsed +=BazaarLoop;
+                _runTimer.AutoReset = true;
+                _runTimer.Start();
+                _botStarted = true;
+                btn.Text = "Stop Bot";
+            }
+            else
+            {
+                _runTimer?.Stop();
+                _botStarted = false;
+                btn.Text = "Start Bot";
+            }
+        }
+
+        private void BazaarLoop(object? sender, ElapsedEventArgs e)
+        {
+            if (_isRunning)
+            {
+                return;
+            }
+
+            _isRunning = true;
+
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                foreach (var item in listBox1.SelectedItems)
+                {
+                    string? selectedName = item.ToString();
+
+                    if (selectedName == null)
+                    {
+                        continue;
+                    }
+
+                    GetReagentLocation(selectedName);
+                }
+            }
+
+            _isRunning = false;
+        }
+
+        private void GetReagentLocation(string itemName)
+        {
+            Point? item = ImageFinder.RetrieveTargetImagePositionInScreenshot($"{StorageUtils.GetAppPath()}/Bazaar/Reagents/{itemName}.png");
+
+            if (item.HasValue)
+            {
+                label1.Text = "Found item";
+                WinAPI.click(item.Value);
+                BuyReagent();
+            }else
+            {
+                label1.Text = "Not ofund";
+            }
+        }
+
+        private void BuyReagent()
+        {
+            Point? buyBtn = ImageFinder.RetrieveTargetImagePositionInScreenshot($"{StorageUtils.GetAppPath()}/Bazaar/buybtn.png");
+
+            while (buyBtn == null)
+            {
+                buyBtn = ImageFinder.RetrieveTargetImagePositionInScreenshot($"{StorageUtils.GetAppPath()}/Bazaar/buybtn.png");
+            }
+
+            if (buyBtn.HasValue)
+            {
+                WinAPI.click(buyBtn.Value);
+
+                Point? yesBtn = ImageFinder.RetrieveTargetImagePositionInScreenshot($"{StorageUtils.GetAppPath()}/Bazaar/yesBtn.png");
+                Point? okBtn = ImageFinder.RetrieveTargetImagePositionInScreenshot($"{StorageUtils.GetAppPath()}/Bazaar/okBtn.png");
+
+                while (yesBtn == null && okBtn == null)
+                {
+                    yesBtn = ImageFinder.RetrieveTargetImagePositionInScreenshot($"{StorageUtils.GetAppPath()}/Bazaar/yesBtn.png");
+                    okBtn = ImageFinder.RetrieveTargetImagePositionInScreenshot($"{StorageUtils.GetAppPath()}/Bazaar/okBtn.png");
+                }
+
+                if (yesBtn.HasValue)
+                {
+                    WinAPI.click(yesBtn.Value);
+                }
+
+                if (okBtn.HasValue)
+                {
+                    WinAPI.click(okBtn.Value);
+                }
+            }
+
+            RefreshShop();
+        }
+
+        private void RefreshShop()
+        {
+            Point? refreshBtn = ImageFinder.RetrieveTargetImagePositionInScreenshot($"{StorageUtils.GetAppPath()}/Bazaar/refreshBtn.png");
+
+            while (refreshBtn == null)
+            {
+                refreshBtn = ImageFinder.RetrieveTargetImagePositionInScreenshot($"{StorageUtils.GetAppPath()}/Bazaar/refreshBtn.png");
+            }
+
+            if (refreshBtn.HasValue)
+            {
+                WinAPI.click(refreshBtn.Value);
+            }
+        }
+    }
+}
