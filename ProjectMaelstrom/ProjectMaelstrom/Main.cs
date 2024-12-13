1225505fe0ec5a24ddc4b689e654aa9503eb0a70
Joseph Williamson <j.williamson@gambler-labs.com>
2024-12-12 19:53:22 -0500

    remove incorrect testing while loop in configuration

--- ProjectMaelstrom/ProjectMaelstrom/Main.cs
+++ ProjectMaelstrom/ProjectMaelstrom/Main.cs
@@ -39,10 +39,6 @@ public partial class Main : Form
 
     private async void startConfigurationBtn_Click(object sender, EventArgs e)
     {
-        while(true)
-        {
-            ImageFinder.IsImageInCenter($"{StorageUtils.GetAppPath()}/Halfang/loading.png");
-        }
         while (!GeneralUtils.Instance.IsGameVisible())
         {
             GeneralUtils.Instance.OpenGameWindow();
